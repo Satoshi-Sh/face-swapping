4213e68e26b0ff055da45562e9643d76b2d0dbed
satoshi-sh <satoss1108@gmail.com>
2023-07-05 19:30:06 -0500

    Implement swap function on streamlit and download button for the newly created image

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
-from utils import detect_faces
+from utils import detect_faces, swap_face,load_model
+import cv2
 import base64
+from io import BytesIO
+from PIL import Image
+
+def convert_image(img):
+    pil_image = Image.fromarray(img)
+    buf = BytesIO()
+    pil_image.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
 
 st.set_page_config(page_title='All Same Face')
 
@@ -10,17 +20,15 @@ st.title("All Same Face")
 st.write("Upload a picture with more than one person")
 st.write("It may take more time if the picture has many faces..")
 
-# Sidebar
-st.sidebar.header("User Input")
-user_input = st.sidebar.text_input("Enter your name", "John Doe")
-
 
 # Show an image
 image = st.file_uploader("Upload an image (jpg,jpeg,png)",type=['jpg', 'jpeg', 'png'])
 if image is not None:
     st.image(image, caption="Uploaded Image", use_column_width=True)
+    # get file name for the new image
+    file_name = image.name
     # detect faces 
-    images = detect_faces(image)
+    images,img,faces = detect_faces(image)
     col1,col2,col3,col4 = st.columns(4)
     options=['choose target image']
     for i,clippedimage in enumerate(images):
@@ -35,9 +43,17 @@ if image is not None:
             col3.image(clippedimage,caption=f"clipped-face {i}")
         if index==0:
             col4.image(clippedimage,caption=f"clipped-face {i}")
-    
-    selected_option = st.selectbox("Select an option", options)
-    if selected_option != 'choose target image':
-        index = int(selected_option.split(' ')[-1]) -1
-        st.image(images[index],caption=" image")
+    if (model :=load_model()):
+       selected_option = st.selectbox("Select an option", options)
+       if selected_option != 'choose target image':
+            index = int(selected_option.split(' ')[-1]) -1
+            st.image(images[index],caption="target face")
+            new_image = swap_face(img,faces,faces[index],model)
+            st.image(new_image,caption='All Same Face')
+            
+            st.download_button("Download Swapped Image", convert_image(new_image),f"swapped_{file_name.split('.')[0]}.png","image/png")
+            
+    else:
+        st.write("Loading trained model")
+
         
--- utils.py
+++ utils.py
@@ -5,10 +5,10 @@ from insightface.app import FaceAnalysis
 
 def main():
     return;
-app = FaceAnalysis(name="buffalo_l")
-app.prepare(ctx_id=0, det_size=(640,640))
 
 def detect_faces(img):
+    app = FaceAnalysis(name="buffalo_l")
+    app.prepare(ctx_id=0, det_size=(640,640))
     file_bytes = np.asarray(bytearray(img.read()), dtype=np.uint8)
     img = cv2.imdecode(file_bytes, 1)
     faces = app.get(img)
@@ -18,7 +18,17 @@ def detect_faces(img):
         bbox = [int(b) for b in bbox]
         clipped_face = img[bbox[1]:bbox[3], bbox[0]:bbox[2], ::-1]
         data.append(clipped_face)
-    return data 
+    return data,img,faces
+
+def load_model():
+    swapper = insightface.model_zoo.get_model('inswapper_128.onnx',download=False,download_zip=False)
+    return swapper
+def swap_face(img,faces,target_face,swapper):
+    res = img.copy()
+    for face in faces:
+        res = swapper.get(res,face,target_face,paste_back=True)
+    return res[:,:,::-1]
+
 
 if __name__ == "__main__":
     main()
